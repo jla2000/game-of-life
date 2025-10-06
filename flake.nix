8914be42eceea0d8418d4207a52ec2c886fb662c
Jan Lafferton <jan@lafferton.de>
2025-10-06 19:29:08 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs = inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "game-of-life";
+        src = pkgs.lib.cleanSource ./.;
+        buildInputs = [ pkgs.raylib ];
+        nativeBuildInputs = [ pkgs.zig ];
+
+        # zig.hook broken:
+        # https://github.com/NixOS/nixpkgs/issues/247719
+        buildPhase = "zig build --global-cache-dir .";
+        installPhase = ''
+          mkdir -p $out/bin
+          mv ./zig-out/bin/* $out/bin/
+        '';
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ pkgs.raylib ];
+        nativeBuildInputs = [ pkgs.zig ];
+      };
+    };
+}
