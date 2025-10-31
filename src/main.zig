c3d734de8fefd1bea6d4512e0f0c952ba2690fcc
Lafferton, Jan <jan.lafferton@vector.com>
2025-10-31 15:49:22 +0100

    Add some neat features

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,7 @@ const c = @cImport({
 const WIN_WIDTH = 1280;
 const WIN_HEIGHT = 720;
 
-const GRID_SCALE = 4;
+const GRID_SCALE = 2;
 const GRID_WIDTH = WIN_WIDTH / GRID_SCALE;
 const GRID_HEIGHT = WIN_HEIGHT / GRID_SCALE;
 
@@ -79,6 +79,8 @@ pub fn main() !void {
     };
     defer c.glfwDestroyWindow(window);
 
+    c.glfwSetInputMode(window, c.GLFW_STICKY_KEYS, c.GLFW_TRUE);
+
     c.glfwMakeContextCurrent(window);
     std.debug.assert(c.epoxy_gl_version() == 45);
 
@@ -93,20 +95,13 @@ pub fn main() !void {
     const frag_shader = try compileShader(c.GL_FRAGMENT_SHADER, @embedFile("fragment.glsl"));
     const render_program = try linkProgram(&[_]c.GLuint{ vert_shader, frag_shader });
 
-    var random_data = std.mem.zeroes([GRID_WIDTH * GRID_HEIGHT]u8);
-    for (&random_data) |*value| {
-        if (std.crypto.random.int(u8) % 2 == 0) {
-            value.* = 255;
-        }
-    }
-
     var texture0: c.GLuint = 0;
     c.glGenTextures(1, &texture0);
     c.glActiveTexture(c.GL_TEXTURE0);
     c.glBindTexture(c.GL_TEXTURE_2D, texture0);
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MIN_FILTER, c.GL_NEAREST);
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MAG_FILTER, c.GL_NEAREST);
-    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_R8, GRID_WIDTH, GRID_HEIGHT, 0, c.GL_RED, c.GL_UNSIGNED_BYTE, &random_data);
+    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_R8, GRID_WIDTH, GRID_HEIGHT, 0, c.GL_RED, c.GL_UNSIGNED_BYTE, null);
     c.glBindImageTexture(0, texture0, 0, c.GL_FALSE, 0, c.GL_READ_WRITE, c.GL_R8);
 
     var texture1: c.GLuint = 0;
@@ -116,7 +111,7 @@ pub fn main() !void {
     c.glBindImageTexture(1, texture1, 0, c.GL_FALSE, 0, c.GL_READ_WRITE, c.GL_R8UI);
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MIN_FILTER, c.GL_NEAREST);
     c.glTexParameteri(c.GL_TEXTURE_2D, c.GL_TEXTURE_MAG_FILTER, c.GL_NEAREST);
-    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_R8, GRID_WIDTH, GRID_HEIGHT, 0, c.GL_RED, c.GL_UNSIGNED_BYTE, &random_data);
+    c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_R8, GRID_WIDTH, GRID_HEIGHT, 0, c.GL_RED, c.GL_UNSIGNED_BYTE, null);
     c.glBindImageTexture(1, texture1, 0, c.GL_FALSE, 0, c.GL_READ_WRITE, c.GL_R8);
 
     const input_data = c.glGetUniformLocation(compute_program, "input_data");
@@ -130,6 +125,9 @@ pub fn main() !void {
     var input_unit: c.GLint = 0;
     var output_unit: c.GLint = 1;
 
+    var running = true;
+    var pressed = false;
+
     while (c.glfwWindowShouldClose(window) == 0) {
         c.glClear(c.GL_COLOR_BUFFER_BIT);
 
@@ -137,18 +135,54 @@ pub fn main() !void {
         c.glUniform1i(c.glGetUniformLocation(render_program, "render_texture"), input_unit);
         c.glDrawArrays(c.GL_TRIANGLE_STRIP, 0, 4);
 
-        c.glUseProgram(compute_program);
-        c.glUniform1i(input_data, input_unit);
-        c.glUniform1i(output_data, output_unit);
-        c.glDispatchCompute(WIN_WIDTH / 10, WIN_HEIGHT / 10, 1);
+        if (running) {
+            c.glUseProgram(compute_program);
+            c.glUniform1i(input_data, input_unit);
+            c.glUniform1i(output_data, output_unit);
+            c.glDispatchCompute(WIN_WIDTH / 10, WIN_HEIGHT / 10, 1);
+            c.glMemoryBarrier(c.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT | c.GL_TEXTURE_FETCH_BARRIER_BIT);
 
-        std.mem.swap(@TypeOf(input_unit), &input_unit, &output_unit);
+            std.mem.swap(@TypeOf(input_unit), &input_unit, &output_unit);
+        }
 
         c.glfwPollEvents();
         c.glfwSwapBuffers(window);
 
         if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS) {
             break;
+        }
+
+        if (c.glfwGetKey(window, c.GLFW_KEY_SPACE) == c.GLFW_PRESS) {
+            if (!pressed) {
+                running = !running;
+                pressed = true;
+            }
+        } else {
+            pressed = false;
+        }
+
+        if (c.glfwGetMouseButton(window, c.GLFW_MOUSE_BUTTON_1) == c.GLFW_PRESS) {
+            var temp_buf: [GRID_WIDTH * GRID_HEIGHT]u8 = undefined;
+
+            c.glBindTexture(c.GL_TEXTURE_2D, if (input_unit == 0) texture0 else texture1);
+            c.glGetTexImage(c.GL_TEXTURE_2D, 0, c.GL_RED, c.GL_UNSIGNED_BYTE, &temp_buf);
+
+            var x: f64 = 0.0;
+            var y: f64 = 0.0;
+            c.glfwGetCursorPos(window, &x, &y);
+
+            const grid_x: usize = @intFromFloat(x / GRID_SCALE);
+            const grid_y: usize = @intFromFloat(GRID_HEIGHT - y / GRID_SCALE);
+            const size = 30;
+
+            for (0..size) |y_off| {
+                for (0..size) |x_off| {
+                    const idx = (grid_y + y_off -| size / 2) * GRID_WIDTH + (grid_x +| x_off -| size / 2);
+                    temp_buf[idx % temp_buf.len] = 255;
+                }
+            }
+
+            c.glTexImage2D(c.GL_TEXTURE_2D, 0, c.GL_R8, GRID_WIDTH, GRID_HEIGHT, 0, c.GL_RED, c.GL_UNSIGNED_BYTE, &temp_buf);
         }
     }
 }
